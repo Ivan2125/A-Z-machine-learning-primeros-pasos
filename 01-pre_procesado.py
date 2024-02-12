8a0ab33c9c155a2a92d665083cb35d08c5657040
Ivan2125 <cristianparra2125@gmail.com>
2024-02-12 13:38:41 -0500

    Se realizó la primera parte del curso de ML - preprocesamiento de datos

---
+++ 01-pre_procesado.py
@@ -0,0 +1,48 @@
+# Herramientas de preprocesamiento
+
+# Importar librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importar el dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Manejo de datos faltantes
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
+
+# Codificar datos categóricos (Este paso normalemente le llaman creación de variables Dummy)
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+# Codificar la variable Purchased (dependiente)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
+# Dividir el dataset en un conjuntos de entrenamiento (training) y otro de evaluación (testing)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+# Escalado de variables
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+print(X_train)
+print(X_test)
